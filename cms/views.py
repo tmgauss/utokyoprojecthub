0aade69dcece41201e11efbcad4a6c185f3e187b
tmgauss <steveforever7822@gmail.com>
2020-03-20 23:43:22 +0900

    Finish static pages

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'cms/index.html', {})
+
+
+def about(request):
+    return render(request, 'cms/about.html', {})
